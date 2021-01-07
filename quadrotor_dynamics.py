ad3c992a6c800351639829dbe678d7e7edb590fb
unknown <onur_akgun42@hotmail.com>
2021-01-07 22:41:06 +0300

    First commit

---
+++ quadrotor_dynamics.py
@@ -0,0 +1,290 @@
+import numpy as np
+from numpy import array, zeros, diag, ones, sin, cos, tan, linspace, dot, pi
+from numpy.random import uniform
+import time
+from scipy import integrate
+from numpy import isnan, pi, isinf
+from numpy.random import normal
+import os
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import lr_scheduler
+import math
+from scipy.spatial.transform import Rotation as R
+
+
+def world_to_body(state, waypoint_world):
+    psi, theta, phi = state[5], state[4], state[3]
+    rot = R.from_euler('zyx', [[psi, theta, phi]], degrees=False).as_dcm()
+    rot = rot.reshape(3,3)
+    # R = np.array([[cos(theta)*cos(psi), sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi), cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi)],
+    #               [cos(theta)*sin(psi), sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi), cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi)],
+    #               [-sin(theta), sin(phi)*cos(theta), cos(phi)*cos(theta)]])
+    waypoint_body = np.dot(rot.T, waypoint_world.reshape(-1,1))
+    
+    return waypoint_body.ravel()
+
+
+def body_to_world(state, waypoint_body):
+    psi, theta, phi = state[5], state[4], state[3]
+    rot = R.from_euler('zyx', [[psi, theta, phi]], degrees=False).as_dcm()
+    # R = np.array([[cos(theta)*cos(psi), sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi), cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi)],
+    #               [cos(theta)*sin(psi), sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi), cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi)],
+    #               [-sin(theta), sin(phi)*cos(theta), cos(phi)*cos(theta)]])
+    waypoint_world = np.dot(rot, waypoint_body.reshape(-1,1))
+    
+    return waypoint_world.ravel()
+
+
+class Quadrotor:
+    
+    def __init__(self, state0, coeff_pos=1.0, coeff_angle = 0.25, coeff_control = 0.0, coeff_final_pos=0.0):
+        
+        self.state = state0
+        self.U = [1, 0., 0., 0.]
+        self.costValue = 0.
+        self.coeff_pos = coeff_pos
+        self.coeff_angle = coeff_angle
+        self.coeff_control = coeff_control
+        self.coeff_final_pos = coeff_final_pos
+        self.Controllers = ["Backstepping_1","Backstepping_2","Backstepping_3","Backstepping_4"]
+        
+    
+    def model_parameters(self):
+        g = 9.81
+        m = 1.52
+        Ixx, Iyy, Izz = 0.0347563, 0.0458929, 0.0977
+        I1 = (Iyy - Izz) / Ixx
+        I2 = (Izz - Ixx) / Iyy
+        I3 = (Ixx - Iyy) / Izz
+        Jr = 0.0001
+        l = 0.09
+        b = 8.54858e-6
+        d = 1.6e-2
+
+        return g, m, Ixx, Iyy, Izz, I1, I2, I3, Jr, l, b, d
+
+
+    def model_dynamics(self, t, state):
+        g, m, Ixx, Iyy, Izz, I1, I2, I3, Jr, l, b, d = self.model_parameters()
+        #states: [x,y,z,phi,theta,psi,x_dot,y_dot,z_dot,phi_dot,theta_dot,psi_dot]
+
+        x,y,z,phi,theta,psi,x_dot,y_dot,z_dot,phi_dot,theta_dot,psi_dot = state
+        U1, U2, U3, U4 = self.U
+
+        # if (d*U1 - 2*d*U3 - b*U4) < 0:
+        #     omega1 = - np.sqrt(- d*U1 + 2*d*U3 + b*U4) / (2*np.sqrt(b*d))
+        # else:
+        #     omega1 = - np.sqrt(d*U1 - 2*d*U3 - b*U4) / (2*np.sqrt(b*d))
+
+        # if (d*U1 - 2*d*U2 + b*U4) < 0:
+        #     omega2 = -np.sqrt(-d*U1 + 2*d*U2 - b*U4) / (2*np.sqrt(b*d))
+        # else:
+        #     omega2 = -np.sqrt(d*U1 - 2*d*U2 + b*U4) / (2*np.sqrt(b*d))
+
+
+        # if (d*U1 + 2*d*U3 - b*U4) < 0:
+        #     omega3 = -np.sqrt(-d*U1 - 2*d*U3 + b*U4) / (2*np.sqrt(b*d))
+        # else:
+        #     omega3 = -np.sqrt(d*U1 + 2*d*U3 - b*U4) / (2*np.sqrt(b*d))
+
+        # if (d*U1 + 2*d*U2 + b*U4) < 0:
+        #     omega4 = -np.sqrt(-d*U1 - 2*d*U2 - b*U4) / (2*np.sqrt(b*d))
+        # else:
+        #     omega4 = -np.sqrt(d*U1 + 2*d*U2 + b*U4) / (2*np.sqrt(b*d))
+
+
+        omega = 0.
+
+        state_dot = np.zeros(12)
+        state_dot[0] = x_dot
+        state_dot[1] = y_dot
+        state_dot[2] = z_dot
+        state_dot[3] = phi_dot
+        state_dot[4] = theta_dot
+        state_dot[5] = psi_dot
+
+        state_dot[6] = (cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi))*U1/m
+        state_dot[7] = (cos(phi)*sin(theta)*sin(psi) - sin(phi)*cos(psi))*U1/m
+        state_dot[8] = -g + cos(phi)*cos(theta)*U1/m    
+        state_dot[9] = theta_dot*psi_dot*I1 - Jr / Ixx * theta_dot * omega  + l/Ixx*U2
+        state_dot[10] = phi_dot*psi_dot*I2 + Jr / Iyy * phi_dot * omega + l/Iyy*U3
+        state_dot[11] = phi_dot*theta_dot*I3 + 1/Izz*U4
+
+        return state_dot
+
+
+    
+
+    def backstepping(self, A1, A2, A3, A4, A5, A6, U_list, ref_traj):
+        g, m, Ixx, Iyy, Izz, I1, I2, I3, Jr, l, b, d = self.model_parameters()
+
+        U1, U2, U3, U4 = U_list
+
+        #self.state: [x,y,z,phi,theta,psi,x_dot,y_dot,z_dot,phi_dot,theta_dot,psi_dot]
+        x, y, z = self.state[0], self.state[1], self.state[2]
+        phi, theta, psi = self.state[3], self.state[4], self.state[5]
+        x_dot, y_dot, z_dot = self.state[6], self.state[7], self.state[8]
+        phi_dot, theta_dot, psi_dot = self.state[9], self.state[10], self.state[11]
+
+    #     ref_traj = [xd[i], yd[i], zd[i], xd_dot[i], yd_dot[i], zd_dot[i], 
+    #                 xd_ddot[i], yd_ddot[i], zd_ddot[i], xd_dddot[i], yd_dddot[i],
+    #                 xd_ddddot[i], yd_ddddot[i], psid[i], psid_dot[i], psid_ddot[i]]
+
+
+        xd, yd, zd = ref_traj[0], ref_traj[1], ref_traj[2], 
+        xd_dot, yd_dot, zd_dot = ref_traj[3], ref_traj[4], ref_traj[5]
+        xd_ddot, yd_ddot, zd_ddot = ref_traj[6], ref_traj[7], ref_traj[8]
+        xd_dddot, yd_dddot = ref_traj[9], ref_traj[10]
+        xd_ddddot, yd_ddddot = ref_traj[11], ref_traj[12]
+        psid, psid_dot, psid_ddot = ref_traj[13], ref_traj[14], ref_traj[15]
+
+        x1, x2, x3 = array([[x], [y]]), array([[x_dot], [y_dot]]), array([[phi], [theta]])
+        x4, x5, x6 = array([[phi_dot], [theta_dot]]), array([[psi], [z]]), array([[psi_dot], [z_dot]])
+
+        g0 = array([[np.cos(psi), np.sin(psi)],  [np.sin(psi), -np.cos(psi)]])
+        g0_inv = array([[np.cos(psi), np.sin(psi)],  [np.sin(psi), -np.cos(psi)]])
+
+        g1 = array([[theta_dot*psi_dot*I1],  [phi_dot*psi_dot*I2]])
+        g2 = array([[phi_dot*theta_dot*I3],  [-g]])
+
+        l0 = array([[np.cos(phi)*np.sin(theta)],  [np.sin(phi)]])*U1/m 
+        dl0_dx3 = array([[-np.sin(phi)*np.sin(theta), np.cos(phi)*np.cos(theta)],  [np.cos(phi), 0]])*U1/m 
+        dl0_dx3_inv = array([[0, 1/np.cos(phi)],  [1/np.cos(theta)*1/np.cos(phi), 1/np.cos(phi)*np.tan(theta)*np.tan(phi)]])*m/U1 
+        dl0_dx3_inv_dot = array([[0, 1/np.cos(phi)*np.tan(phi)*phi_dot], 
+                                 [1/np.cos(theta)*1/np.cos(phi)*(np.tan(theta)*theta_dot + np.tan(phi)*phi_dot), 1/np.cos(phi)*((1/np.cos(theta))**2*np.tan(phi)*theta_dot + (-1+2*(1/np.cos(phi))**2)*np.tan(theta)*phi_dot)]])*m/U1 
+
+    #     Omega_square = Omega_coef_inv * abs([U1/b  U2/b  U3/b  U4/d]) 
+    #     Omega_param = sqrt(Omega_square) 
+    #     omega = Omega_param(2) + Omega_param[3] - Omega_param(1) - Omega_param(3) 
+
+    #     h1 = [-Jr/Ixx*theta_dot*omega  Jr/Iyy*phi_dot*omega] 
+        h1 = 0 
+        k1 = diag([l/Ixx, l/Iyy]) 
+        k1_inv = diag([Ixx/l, Iyy/l]) 
+        k2 = diag([1/Izz, np.cos(phi)*np.cos(theta)/m]) 
+        k2_inv = diag([Izz, m/(np.cos(phi)*np.cos(theta))]) 
+
+        x1d = array([[xd], [yd]])  
+        x1d_dot = array([[xd_dot], [yd_dot]]) 
+        x1d_ddot = array([[xd_ddot], [yd_ddot]]) 
+        x1d_dddot = array([[xd_dddot], [yd_dddot]]) 
+        x1d_ddddot = array([[xd_ddddot], [yd_ddddot]]) 
+
+        x5d = array([[psid], [zd]])
+        x5d_dot = array([[psid_dot], [zd_dot]]) 
+        x5d_ddot = array([[psid_ddot], [zd_ddot]]) 
+
+        z1 = x1d - x1 
+        v1 = x1d_dot + dot(A1,z1) 
+        z2 = v1 - x2 
+        z1_dot = -dot(A1,z1) + z2 
+        v1_dot = x1d_ddot + dot(A1,z1_dot) 
+        v2 = dot(g0_inv, z1 + v1_dot + dot(A2,z2)) 
+        z3 = v2 - l0  
+        z2_dot = -z1 - dot(A2,z2) + dot(g0,z3) 
+        z1_ddot = -dot(A1,z1_dot) + z2_dot 
+        v1_ddot = x1d_dddot + dot(A1, z1_ddot) 
+        v2_dot = dot(g0_inv, z1_dot + v1_ddot + dot(A2,z2_dot)) 
+        v3 = dot(dl0_dx3_inv, dot(g0.T,z2) + v2_dot + dot(A3, z3)) 
+        z4 = v3 - x4 
+        z3_dot = -dot(g0.T, z2) - dot(A3,z3) + dot(dl0_dx3, z4) 
+        z2_ddot = - z1_dot - dot(A2, z2_dot) + dot(g0, z3_dot) 
+        z1_dddot = -dot(A1, z1_ddot) + z2_ddot 
+        v1_dddot = x1d_ddddot + dot(A1, z1_dddot) 
+        v2_ddot = dot(g0_inv, z1_ddot + v1_dddot + dot(A2, z2_ddot)) 
+        v3_dot = dot(dl0_dx3_inv, dot(g0.T, z2_dot) + v2_ddot + dot(A3, z3_dot)) + dot(dl0_dx3_inv_dot, dot(g0.T, z2) + v2_dot + dot(A3, z3))
+        l1 = dot(k1_inv, dot(dl0_dx3.T, z3) + v3_dot - g1 - h1 + dot(A4, z4)).ravel()
+
+        z5 = x5d - x5 
+        v5 = x5d_dot + dot(A5, z5) 
+        z6 = v5 - x6 
+        z5_dot = - dot(A5, z5) + z6 
+        v5_dot = x5d_ddot + dot(A5, z5_dot) 
+        l2 = dot(k2_inv, z5 + v5_dot - g2 + dot(A6, z6)).ravel()
+
+        U1, U2, U3, U4 = l2[1], l1[0], l1[1], l2[0]
+
+        U1 = np.clip(U1, 1.0, 1e2)
+        U2 = np.clip(U2, -1e2, 1e2)
+        U3 = np.clip(U3, -1e2, 1e2)
+        U4 = np.clip(U4, -1e2, 1e2)
+
+        U = np.array([U1, U2, U3, U4])
+
+        return U
+
+    def get_control_input(self, cont, current_traj):
+        U0 = self.U 
+        if (cont == self.Controllers[0]): #Backstepping_1
+            A1, A2, A3 = 15*diag([1,1]), 10*diag([1,1]), 15*diag([1,1]) 
+            A4, A5, A6 = 10*diag([1,1]), 15*diag([1,1]), 10*diag([1,1]) 
+            U = self.backstepping(A1, A2, A3, A4, A5, A6, U0, current_traj) 
+        elif (cont == self.Controllers[1]): #Backstepping_2
+            A1, A2, A3 = 10*diag([1,1]), 5*diag([1,1]), 10*diag([1,1]) 
+            A4, A5, A6 = 5*diag([1,1]), 10*diag([1,1]), 5*diag([1,1])
+            U = self.backstepping(A1, A2, A3, A4, A5, A6, U0, current_traj) 
+        elif (cont == self.Controllers[2]): #Backstepping_3
+            A1, A2, A3 = 5*diag([1,1]), 3*diag([1,1]), 10*diag([1,1]) 
+            A4, A5, A6 = 7*diag([1,1]), 1*diag([1,1]), 1*diag([1,1])  
+            U = self.backstepping(A1, A2, A3, A4, A5, A6, U0, current_traj)
+        elif (cont == self.Controllers[3]): #Backstepping_4
+            A1, A2, A3 = 2*diag([1,1]), 5*diag([1,1]), 2*diag([1,1]) 
+            A4, A5, A6 = 5*diag([1,1]), 2*diag([1,1]), 5*diag([1,1]) 
+            U = self.backstepping(A1, A2, A3, A4, A5, A6, U0, current_traj)
+        return U
+
+
+    def calculate_cost(self, target, final_target=None, final_calculation = False):
+        xd, yd, zd, psid = target
+        
+
+        position_tracking_error = (xd-self.state[0])**2 + (yd-self.state[1])**2 + (zd-self.state[2])**2
+        angular_error = (np.abs(psid-self.state[5])-np.pi/2)**2 #in perfect conditions, difference between yaw angles of gate and drone should be pi/2
+        cont_input = self.U[0]**2 + self.U[1]**2 + self.U[2]**2 + self.U[3]**2
+        
+        if final_calculation:
+            self.costValue += (self.coeff_pos*position_tracking_error + 
+                            self.coeff_angle*angular_error + self.coeff_control*cont_input)
+        else:
+            xf, yf, zf = final_target
+            pos_final_error = (xf-self.state[0])**2 + (yf-self.state[1])**2 + (zf-self.state[2])**2
+            self.costValue += (self.coeff_pos*position_tracking_error + 
+                            self.coeff_angle*angular_error + 
+                            self.coeff_control*cont_input +
+                            self.coeff_final_pos*pos_final_error)
+                        
+
+
+
+    def simulate(self, current_traj, dtau=1e-3, method="Backstepping_3"):
+    #     states: [x,y,z,phi,theta,psi,x_dot,y_dot,z_dot,phi_dot,theta_dot,psi_dot]
+        std_list = [0, 0, 0, 0]
+        r_std, phi_std, theta_std, psi_std = std_list[0], std_list[1], std_list[2], std_list[3]
+        fail_check = False
+
+        ## Add noise, if you wish ##
+        self.state[6] = normal(self.state[6], 0*r_std / 3.0)
+        self.state[7] = normal(self.state[7], 0*r_std / 3.0)
+        self.state[8] = normal(self.state[8], 0*r_std / 3.0)
+        self.state[9] = normal(self.state[9], 0*phi_std)
+        self.state[10] = normal(self.state[10], 0*theta_std)
+        self.state[11] = normal(self.state[11], 0*psi_std)
+
+        U = self.get_control_input(method, current_traj)
+
+        sol = integrate.solve_ivp(fun=self.model_dynamics, t_span=(0, dtau), y0=self.state)
+        self.state = sol.y[:,-1]
+        self.U = U
+
+        if (np.abs(self.state[3]) > np.pi/2)  | (np.abs(self.state[4]) > np.pi/2):
+            self.costValue = 1e12
+            fail_check = True
+            print ("Drone has crashed!")
+        else:
+            target = [current_traj[0], current_traj[1], current_traj[2], current_traj[13]]
+
+        
+        return fail_check
---
+++ quadrotor_formation.py
@@ -0,0 +1,269 @@
+import gym
+from gym import spaces, error, utils
+from gym.utils import seeding
+import numpy as np
+import configparser
+from os import path
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import gca
+from sklearn.neighbors import NearestNeighbors
+import itertools
+import random
+import pdb
+from quadrotor_dynamics import Quadrotor
+
+font = {'family': 'sans-serif',
+        'weight': 'bold',
+        'size': 14}
+
+
+class QuadrotorFormation(gym.Env):
+
+    def __init__(self):
+
+        config_file = path.join(path.dirname(__file__), "formation_flying.cfg")
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        config = config['flock']
+
+        self.dynamic = True # if the agents are moving or not
+        self.mean_pooling = False # normalize the adjacency matrix by the number of neighbors or not
+        #self.degree =  4 # number of nearest neighbors (if 0, use communication range instead)
+        self.degree = 1
+        # number of features per agent
+        self.n_features = 12
+        # number states per agent
+        self.nx_system = self.n_features + 3
+        # number of actions per agent which are desired positions and yaw angle
+        self.n_action = 3
+        self.x_lim = 50.0 # figure x limit
+        self.y_lim = 50.0 # figure y limit
+        
+
+        # problem parameters from file
+        self.n_agents = 4
+        self.comm_radius = float(config['comm_radius'])
+        self.comm_radius2 = self.comm_radius * self.comm_radius
+        self.dt = float(config['system_dt'])
+        self.v_max = float(config['max_vel_init'])
+        self.v_bias = self.v_max
+        self.r_max = float(config['max_rad_init'])
+        self.std_dev = float(config['std_dev']) * self.dt
+
+        state0 = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.]
+        self.quadrotors = [Quadrotor(state0)]*self.n_agents
+
+        # intitialize state matrices
+        self.total_states = np.zeros((self.n_agents, self.nx_system))
+        self.agent_features = np.zeros((self.n_agents, self.n_action)) 
+        self.diff_target = np.zeros((self.n_agents, self.n_action))
+
+        self.a_net = np.zeros((self.n_agents, self.n_agents))
+
+        self.max_action = 50
+        self.gain = 1.0 # TODO - adjust if necessary - may help the NN performance
+        self.action_space = spaces.Box(low=-self.max_action, high=self.max_action, shape=(self.n_action*self.n_agents,),dtype=np.float32)
+
+        self.observation_space = spaces.Box(low=-np.Inf, high=np.Inf, shape=(self.n_agents, self.n_features),
+                                            dtype=np.float32)
+
+        self.fig = None
+        self.line1 = None
+        self.counter = 0
+        self.seed()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, action):
+        #self.nu = 1
+        self.agent_targets = np.reshape(action,(self.n_agents, self.n_action))
+        self.fail_check = np.zeros(self.n_agents)
+        eps = 1.5
+        done = False
+
+        for i in range(self.n_agents):
+            #     ref_traj = [xd[i], yd[i], zd[i], xd_dot[i], yd_dot[i], zd_dot[i], 
+            #                 xd_ddot[i], yd_ddot[i], zd_ddot[i], xd_dddot[i], yd_dddot[i],
+            #                 xd_ddddot[i], yd_ddddot[i], psid[i], psid_dot[i], psid_ddot[i]]
+            xd, yd, zd = self.agent_targets[i][0], self.agent_targets[i][1], self.agent_targets[i][2]
+            ref_traj = [xd, yd, zd, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            self.fail_check[i] = int(self.quadrotors[i].simulate(ref_traj))
+            self.diff_target[i][:] = self.quadrotors[i].state[0:3]-self.agent_targets[i][:]
+
+       
+
+        if np.sum(self.fail_check) > 0:
+            done = True
+            reward = -1e4
+            return self._get_obs(), reward, done, {}
+
+        if np.sum(self.diff_target[:,0]) < eps and np.sum(self.diff_target[:,1]) < eps and np.sum(self.diff_target[:,2]) < eps:
+            done = True
+
+        return self._get_obs(), self.instant_cost(), done, {}
+
+    def instant_cost(self):  # sum of differences in velocities
+        cost = 0.
+        for i in range(self.n_agents):
+            for j in range(3):
+                cost += (self.agent_targets[i,j] - self.quadrotors[i].state[j])**2
+
+        return -np.sqrt(cost)
+
+
+    def reset(self):
+        x = np.zeros((self.n_agents, 2 * self.n_action)) 
+        self.agent_features = np.zeros((self.n_agents, self.n_action)) 
+        degree = 0
+        min_dist = 0
+        min_dist_thresh = 0.01  # 0.25
+
+        self.counter = 0
+        self.agent_pos_goal = np.zeros((self.n_agents, self.n_action))
+        self.agent_pos_start = np.zeros((self.n_agents, self.n_action))
+
+        eps = 1.0
+        goal_locations = [(-self.x_lim+eps, -self.y_lim+eps, 5), (-self.x_lim+eps, self.y_lim-eps, 5), 
+                          (self.x_lim-eps, self.y_lim-eps, 5), (self.x_lim-eps, -self.y_lim+eps, 5)]
+
+        for i in range(0,self.n_agents):
+            x_target, y_target, z_target = goal_locations[i]
+            self.agent_pos_goal[i,:] = [x_target, y_target, z_target]
+            
+        #scheme :
+        #space all agents in a frontier that looks like -> .'.
+        #this means everyone has x goal position separated by two units.
+        #and y goal position varies
+        # let the number of agents be odd. for kosher reasons.
+        #hack, say n agents = 3. Then placer_x is = -2
+        self.placer_x = (self.n_agents/2)*2*(-1)
+
+        ##########declare start positions############
+        for i in range(0,self.n_agents):
+            x_start = self.placer_x + 2*i
+            y_start = np.random.uniform(-1.,0)
+            z_start = np.random.uniform(0,2.)
+
+            self.agent_pos_start[i,:] = [x_start, y_start, z_start]
+            
+
+        x[:,0] = self.agent_pos_start[:,0]
+        x[:,1] = self.agent_pos_start[:,1]
+        x[:,2] = self.agent_pos_start[:,2]
+
+        x[:,3] = self.agent_pos_goal[:,0]
+        x[:,4] = self.agent_pos_goal[:,1]
+        x[:,5] = self.agent_pos_goal[:,2]
+
+        # compute distances between agents
+        a_net = self.dist2_mat(x)
+
+        # compute minimum distance between agents and degree of network to check if good initial configuration
+        min_dist = np.sqrt(np.min(np.min(a_net)))
+        a_net = a_net < self.comm_radius2
+        degree = np.min(np.sum(a_net.astype(int), axis=1))
+
+        self.x = x
+
+        self.a_net = self.get_connectivity(self.x)
+
+        init_action = self.agent_pos_goal
+
+        #pdb.set_trace()
+        return self._get_obs()
+
+    def _get_obs(self):
+
+        for i in range(self.n_agents):
+            self.agent_features[i,0] = self.quadrotors[i].state[0] - self.agent_pos_goal[i,0]
+            self.agent_features[i,1] = self.quadrotors[i].state[1] - self.agent_pos_goal[i,1]
+            self.agent_features[i,2] = self.quadrotors[i].state[2] - self.agent_pos_goal[i,2]
+
+        if self.dynamic:
+            state_network = self.get_connectivity(self.x)
+        else:
+            state_network = self.a_net
+
+        #return (state_values, state_network)
+        return self.agent_features
+
+    def dist2_mat(self, x):
+
+        x_loc = np.reshape(x[:,0:3], (self.n_agents,3,1))
+        a_net = np.sum(np.square(np.transpose(x_loc, (0,2,1)) - np.transpose(x_loc, (2,0,1))), axis=2)
+        np.fill_diagonal(a_net, np.Inf)
+        return a_net
+
+
+    def get_connectivity(self, x):
+
+
+        if self.degree == 0:
+            a_net = self.dist2_mat(x)
+            a_net = (a_net < self.comm_radius2).astype(float)
+        else:
+            neigh = NearestNeighbors(n_neighbors=self.degree)
+            neigh.fit(x[:,3:6])
+            a_net = np.array(neigh.kneighbors_graph(mode='connectivity').todense())
+
+        if self.mean_pooling:
+            # Normalize the adjacency matrix by the number of neighbors - results in mean pooling, instead of sum pooling
+            n_neighbors = np.reshape(np.sum(a_net, axis=1), (self.n_agents,1)) # TODO or axis=0? Is the mean in the correct direction?
+            n_neighbors[n_neighbors == 0] = 1
+            a_net = a_net / n_neighbors
+
+        return a_net
+
+
+    def render(self, mode='human'):
+        """
+        Render the environment with agents as points in 2D space
+        """
+
+        edge_list = []
+        adj_matrix = self.get_connectivity(self.x)
+
+        num_ag = self.n_agents
+        for i in range(0,num_ag):
+            for j in range(0,num_ag):
+                if adj_matrix[i][j] > 0:
+                    edge_list.append((i,j))
+
+        edge_list = np.array(edge_list)
+
+
+        if self.fig is None:
+            plt.ion()
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            #line1, = ax.plot(self.x[:, 0], self.x[:, 1], linestyle='-', color='y',markerfacecolor='blue', marker='o')  # Returns a tuple of line objects, thus the comma
+            line1, = ax.plot(self.x[:, 0], self.x[:, 1], 'bo')  # Returns a tuple of line objects, thus the comma
+            #line1 = ax.plot(x[edge_list.T], y[edge_list.T], linestyle='-', color='y',markerfacecolor='red', marker='o')
+
+            ax.plot([0], [0], 'kx')
+            ax.plot(self.agent_pos_start[:,0], self.agent_pos_start[:,1], 'kx')
+            ax.plot(self.agent_pos_goal[:,0], self.agent_pos_goal[:,1], 'rx')
+
+            # plt.ylim(-15* self.r_max, 15.0 * self.r_max)
+            # plt.xlim(-10.0 * self.r_max, 10.0 * self.r_max)
+            plt.ylim(-self.y_lim, self.y_lim)
+            plt.xlim(-self.x_lim, self.x_lim)
+            a = gca()
+            a.set_xticklabels(a.get_xticks(), font)
+            a.set_yticklabels(a.get_yticks(), font)
+            plt.title('%d Robots Formation'%self.n_agents)
+            self.fig = fig
+            self.line1 = line1
+
+            plt.gca().legend(('Robots'))
+        self.line1.set_xdata(self.x[:, 0])
+        self.line1.set_ydata(self.x[:, 1])
+
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
+    def close(self):
+        pass
+
