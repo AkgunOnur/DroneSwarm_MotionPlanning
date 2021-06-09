61340ea10ffde0949d9654be9d21c66bb1c95f38
Kamil Canberk Atik <onur_akgun42@hotmail.com>
2021-06-09 15:57:16 +0300

    V100 Training Done

---
+++ ic3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH -p longq        # İşin çalıştırılması istenen kuyruk seçilir
+#SBATCH -o %j.out      # Çalıştırılan kodun ekran çıktılarını içerir
+#SBATCH -e %j.err      # Karşılaşılan hata mesajlarını içerir
+#SBATCH -n 1           # Talep edilen işlemci  çekirdek sayısı
+
+source activate ic3
+
+python /okyanus/users/deepdrone/DroneSwarm_MotionPlanning/main.py --env_name predator_prey --nagents 5 --nprocesses 1 --num_epochs 20000 --hid_size 128 --detach_gap 10 --lrate 0.001 --max_steps 400 --ic3net --recurrent --scenario planning
