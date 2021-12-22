7ce5c8f389941e4039396a12d10c556bca802ce8
drone-AI <mehmetcankaymaz10@gmail.com>
2021-12-22 16:17:53 +0300

    Animation and requirements updated

--- animation.py
+++ animation.py
@@ -2,7 +2,7 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description='Animation Control')
-parser.add_argument('--scenario', default='predator', type=str, help='animation scenario [planning, predator]')
+parser.add_argument('--scenario', default='predator', type=str, help='animation scenario [planning, predator planning-uncertainty]')
 args = parser.parse_args()
 
 print(args)
@@ -12,5 +12,7 @@ if __name__ == "__main__":
         os.system('python planning_animate.py')
     elif args.scenario=='predator':
         os.system('python predator_animate.py')
+    elif args.scenario=='planning-uncertainty':
+        os.system('python uncertainty_animation.py')        
     else:
         print("Please enter a valid animation scenario")
